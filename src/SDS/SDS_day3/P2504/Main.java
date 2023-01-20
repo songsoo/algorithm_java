45d645e179b1ee60f189b6953b0af87ccc0a647b
ssh102525 <ssh102525@naver.com>
2023-01-20 15:56:35 +0900

    2023_01_20

--- src/SDS/SDS_day3/P2504/Main.java
+++ src/SDS/SDS_day3/P2504/Main.java
@@ -1,6 +1,7 @@
 package SDS.SDS_day3.P2504;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
@@ -8,42 +9,78 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws Exception{
 
+        System.setIn(new FileInputStream("E:\\SSAFY9\\intelliJ_workspace\\algorithm_java\\src\\SDS\\SDS_day3\\P2504\\input.txt"));
+
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
 
-        String str = st.nextToken()+" ";
+        String str = st.nextToken();
 
         Stack<Character> strStk = new Stack<Character>();
-        Stack<Integer> intStk = new Stack<Integer>();
-        int sum = 0;
-        int mulp = 0;
+        char cur=' ';
+        int sum =0;
+        int mulValue = 1;
+        int stkCnt = 0;
         for (int i = 0; i < str.length(); i++) {
-            char cur = str.charAt(i);
 
+            char prev = cur;
+            cur = str.charAt(i);
 
-
-            if(cur==')' || cur==']'){
-
+            if(cur=='('){
+                mulValue *= 2;
+                strStk.add(')');
+                stkCnt++;
+            }else if(cur=='['){
+                strStk.add(']');
+                mulValue *= 3;
+                stkCnt++;
+            }else if(cur==')'){
+                if(stkCnt>0) {
+                    if (!(strStk.pop() == ')')) {
+                        sum = -1;
+                        break;
+                    }
+                    if (prev == '(') {
+                        sum += mulValue;
+                    }
+                }else{
+                    sum=-1;
+                    break;
+                }
+                stkCnt--;
+                mulValue/=2;
+            }else if(cur==']'){
+                if(stkCnt>0) {
+                    if (!(strStk.pop() == ']')) {
+                        sum = -1;
+                        break;
+                    }
+                    if (prev == '[') {
+                        sum += mulValue;
+                    }
+                }else{
+                    sum=-1;
+                    break;
+                }
+                stkCnt--;
+                mulValue/=3;
             }else{
-                strStk.push(cur);
+                sum = -1;
+                break;
             }
 
         }
 
+        if(stkCnt!=0){
+            sum = -1;
+        }
+
+        if(sum==-1){
+            System.out.println(0);
+        }else{
+            System.out.println(sum);
+        }
+
     }
 
-    public static boolean checkSame(char a, char b){
-        if(a>b){
-            char tmp = a;
-            a = b;
-            b = tmp;
-        }
-        if(a=='('&&b==')'){
-            return true;
-        }
-        if(a=='['&&b==']'){
-            return true;
-        }
-        return false;
-    }
 }
--- src/Test/Test.java
+++ src/Test/Test.java
@@ -9,9 +9,34 @@ import java.util.*;
 
 public class Test {
 
+    static int n=5;
+    static boolean[][] arr;
+    static int[] arr2;
     public static void main(String[] args) {
 
-        System.out.println((int)'('+" "+(int)')');
+        arr = new boolean[n][n];
+        arr2 = new int[3];
+        dfs(0);
+    }
+
+    static void dfs(int wallCnt){
+        if(wallCnt == 3){
+            for (int i = 0; i < wallCnt; i++) {
+                System.out.print(arr2[i]+" ");
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(!arr[i][j]){
+                    arr[i][j] = true;
+                    arr2[wallCnt] = i*10+j;
+                    dfs(wallCnt+1);
+                    arr[i][j] = false;
+                }
+            }
+        }
     }
 
 }
